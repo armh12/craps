e859e8c28e3ad97e9b645dfe8458204ef9be17d9
armh12 <hovhannisyanarman062@gmail.com>
2023-07-07 15:22:09 +0400

    Initial commit

---
+++ craps-game.py
@@ -0,0 +1,34 @@
+import random
+            
+def game():
+    def random_nums():
+        players_roll = random.randint(1,6)
+        casinos_roll = random.randint(1,6)
+        sum_of_both = players_roll + casinos_roll
+        return sum_of_both
+    p_win_combinations = [7,11]
+    c_win_combinations = [2,3,12]
+    sum_of_both = random_nums()
+    if sum_of_both in p_win_combinations:
+        print(f'The sum of dice is {sum_of_both}')
+        print('Player wins!')
+    elif sum_of_both in c_win_combinations:
+        print(f'The sum of dice is {sum_of_both}')
+        print('Casino wins!')
+    else:
+        print(f'Your goal number is {sum_of_both}!')
+        while True:
+            goal_sum = random_nums()
+            if goal_sum == sum_of_both:
+                print(f'The sum of dice is {goal_sum}')
+                print('Player wins!')
+                break
+            elif goal_sum == 7:
+                print(f'The sum of dice is {goal_sum}')
+                print('Casino wins!')
+                break
+            else:
+                print(f'The sum of dice is {goal_sum}')
+                continue 
+
+game()
